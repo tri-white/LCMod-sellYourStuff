3b3fcb6fa12fbbe4f1e55d633ae8a9fd9b02dc64
tri-white <olegovic101@gmail.com>
2024-01-06 15:17:33 +0200

    init template project

---
+++ SellYourStuff/Patches/ItemPatch.cs
@@ -0,0 +1,23 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellYourStuff.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class ItemPatch
+    {
+        // nameof(PlayerControllerB.Update - if it's not private, this way is better. Otherwise:
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void infiniteSprintPatch(ref float __sprintMeter)
+        {
+            __sprintMeter = 1f;
+            GameNetworkManager.Instance.localPlayerController;
+        }
+    }
+}
---
+++ SellYourStuff/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SellYourStuff.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SellYourStuff
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SellYourStuffModBase : BaseUnityPlugin
+    {
+        // mod configurations for BepInEx library
+        private const string modGUID = "Axeron.SellYourStuff";
+        private const string modName = "SellYourStuff";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        internal ManualLogSource mls;
+
+        private static SellYourStuffModBase Instance;
+
+        void Awake()
+        {
+            // May be useless
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo($"SellYourStuff plugin loaded. Version {modVersion}");
+
+            harmony.PatchAll(typeof(SellYourStuffModBase));
+            harmony.PatchAll(typeof(ItemPatch));
+        }
+
+    }
+}
